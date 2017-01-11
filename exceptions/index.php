2152c82ded94e2aba6c264c5365c270c50ed8f6f
Tatyana <audtg@yandex.ru>
2017-01-11 07:18:34 +0300

    commit

--- exceptions/index.php
+++ exceptions/index.php
@@ -1,21 +1,30 @@
 <?php
 
 $host = 'localhost:C:\Program Files\Firebird\Firebird_2_5\data\test.fdb';
-$username = 'sysdba';
+$username = 'sysdbaqq';
 $password = 'masterkey';
 
 error_reporting(E_ERROR | E_CORE_ERROR | E_USER_ERROR);
 
 $dbh = ibase_connect($host, $username, $password, 'utf-8');
 
+
+var_dump(intval(ibase_errcode()));
+var_dump(strval(ibase_errmsg()));
+echo 'ibase_errmsg()='.ibase_errmsg() . '<br>';
+
 if (!$dbh) {
-   echo 'connection error';
+    echo 'connection error<br>';
+    echo ibase_errcode() . '<br>';
+    echo ibase_errmsg() . '<br>';
 }
 
-$preparedSQL = ibase_prepare('insert into corr values (1, \'haixhajxabjk\')');
+$preparedSQL = ibase_prepare('insert into corr values (\'gfuakga\', \'haixhajxabjk\')');
 
 if (!$preparedSQL) {
-    echo 'preparing error';
+    echo 'preparing error'. '<br>';
+    echo ibase_errcode() . '<br>';
+    echo ibase_errmsg() . '<br>';
 }
 
 $sth = ibase_execute($preparedSQL);
@@ -23,7 +32,11 @@ $sth = ibase_execute($preparedSQL);
 var_dump(gettype($sth));
 
 if (is_integer($sth)) {
-    echo 'inserted '.$sth.' strings';
+    echo 'inserted ' . $sth . ' strings';
+} elseif(!$sth) {
+        echo 'execution error'. '<br>';
+        echo ibase_errcode() . '<br>';
+        echo ibase_errmsg() . '<br>';
 }
 
 $preparedSQL = ibase_prepare('update corr set CORR_ID =  3 where CORR_ID=1');
@@ -37,7 +50,7 @@ $sth = ibase_execute($preparedSQL);
 var_dump(gettype($sth));
 
 if (is_integer($sth)) {
-    echo 'updated '.$sth.' strings';
+    echo 'updated ' . $sth . ' strings';
 } elseif ($sth) {
     echo 'no strings for update';
 } else {
